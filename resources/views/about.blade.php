ec7fb14605a19ddf9fc4f3bbcd4d86d269bafb91
akmalm007 <akmalmaulana915@gmail.com>
2023-12-04 22:37:25 +0700

    feat: img and about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,8 +9,17 @@
 </head>
 
 <body>
-    <h1>Welcome to About</h1>
-    <script src="js/script.js"></script>
+    <h1>About Us</h1>
+    <p>UPDL Jakarta merupakan salah satu unit di bawah PLN Corporate University
+        yang bertanggung jawab dalam menyelanggarakan pembelajaran pada seluruh pegawai
+        Group
+    </p>
+    <h2>Visi</h2>
+    <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
+    <h2>Misi</h2>
+    <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
+    <img src="img/963.jpg" alt="Mobil Balap" width="200">
 </body>
+
 
 </html>
