631a77e31197e7fdceb00c4f431351c6497d6419
akmalm007 <akmalmaulana915@gmail.com>
2023-12-05 12:15:29 +0700

    testing in Arch

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,6 +19,7 @@
     <h2>Misi</h2>
     <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
     <img src="img/963.jpg" alt="Mobil Balap" width="200">
+    <h3>Harapan</h3>
 </body>
 
 
