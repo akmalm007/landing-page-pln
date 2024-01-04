6a31350289a762330a2ff340ad620c39c221512f
akmalm007 <akmalmaulana915@gmail.com>
2024-01-04 22:26:50 +0700

    feat: Adding Maps and UI Element for Facilities and Contact still raw idea not really well cooked yet but in the weeks i will make good enough and hopefull adnan will help me soon since i am suck at styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.main')
 @section('container')
 <div class="card text-bg-dark">
-    <img src="img/img.jpg" style="width: 1300px; height: 400px;" class="card-img" alt="...">
+    <img src="img/963.jpg" style="width: 1100px; height: 400px;" class="card-img" alt="...">
     <div class="card-img-overlay">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-            This content is a little bit longer.</p>
-        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+        <h5 class="card-title">PLN UPDL Jakarta</h5>
+        <p class="card-text">
+            PLN UPDL Jakarta adalah tempat untuk menuntut ilmu bagi para peserta dari perushaan PLN
+            ataupun non-PLN
+        </p>
     </div>
 </div>
 <h1>Halaman Home</h1>
 <ol>
-    <li>List yang perlu dilakukan selama development</li>
-    <li>Coba develop sampai bagia migration</li>
-    <li>Hostig di Google Cloud dengan Compute Engine</li>
-    <li>Buat Github action agar CI/CD</li>
+    <li>Tambah sesuatu untuk UI Home</li>
 </ol>
 @endsection
